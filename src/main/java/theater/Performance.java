352ef4718aa237d78ad668cdce098543ea82ccc0
Xreflextion <lillian.ye06@gmail.com>
2025-11-08 22:10:57 -0500

    Task 1.1 public to private attributes for Performance class

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -5,11 +5,27 @@ package theater;
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public void setPlayID(String newPlayID) {
+        playID = newPlayID;
+    }
+
+    public int getAudience() {
+        return audience;
+    }
+
+    public void setAudience(int newAudience) {
+        audience = newAudience;
     }
 }
